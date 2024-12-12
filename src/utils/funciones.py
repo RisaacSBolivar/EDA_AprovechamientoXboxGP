26382e2f0cd04ade7f72e7a2c252ca582a90f108
RisaacSBolivar <isaacsantoyo@gmail.com>
2024-12-12 14:32:16 -0600

    Análisis bivariante
    Se hizo análisis bivariante y se generaron nuevas columnas categóricas

--- src/utils/funciones.py
+++ src/utils/funciones.py
@@ -32,3 +32,39 @@ def plot_numerical_histograms(df, numerical_columns, bins_list, kde=False):
 
     plt.tight_layout()
     plt.show()
+
+def categorize_score(score, bins, labels):
+    return pd.cut(score, bins=bins, labels=labels, include_lowest=True)
+
+def plot_categorical_numerical_histograms(df, cat_col, num_col):
+    unique_values = df[cat_col].unique()
+    num_unique_values = len(unique_values)
+    num_cols = 3
+    num_rows = (num_unique_values + num_cols - 1) // num_cols
+    
+    fig, axes = plt.subplots(num_rows, num_cols, figsize=(15, 5 * num_rows))
+    axes = axes.flatten()
+    
+    for i, value in enumerate(unique_values):
+        subset = df[df[cat_col] == value]
+        sns.histplot(subset[num_col], ax=axes[i], kde=True)
+        axes[i].set_title(f'{value}')
+    
+    fig.suptitle(f'Histograma de {num_col} por {cat_col}')
+
+    for j in range(i + 1, len(axes)):
+        fig.delaxes(axes[j])
+    
+    plt.tight_layout()
+    plt.show()
+
+def plot_categorical_numerical_boxplots(df, categorical_columns, numerical_columns):
+    for cat_col in categorical_columns:
+        for num_col in numerical_columns:
+            plt.figure(figsize=(10, 6))
+            sns.boxplot(x=df[cat_col], y=df[num_col])
+            plt.title(f'Boxplot de {num_col} por {cat_col}')
+            plt.xlabel(cat_col)
+            plt.ylabel(num_col)
+            plt.xticks(rotation=45)
+            plt.show()
